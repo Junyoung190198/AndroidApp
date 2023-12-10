0f4fecdba21a1e5ba63c91e06a61cf3698e565b7
Jun Young <kevan.maoui@gmail.com>
2023-12-11 03:36:28 +0900

    Finished - Simple SMS message capturing

--- app/src/main/java/com/myapp/myapplication/MainActivity.kt
+++ app/src/main/java/com/myapp/myapplication/MainActivity.kt
@@ -1,18 +1,113 @@
+// MainActivity.kt
 package com.myapp.myapplication
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(){
+    private val SMS_PERMISSION_CODE = 123
 
-    private lateinit var smsTextView: TextView
+    private val smsReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "android.provider.Telephony.SMS_RECEIVED") {
+                val bundle = intent.extras
+                if (bundle != null) {
+                    val pdus = bundle["pdus"] as Array<*>?
+                    if (pdus != null) {
+                        for (i in pdus.indices) {
+                            val format = bundle.getString("format")
+                            val smsMessage = SmsMessage.createFromPdu(pdus[i] as ByteArray, format)
+                            val senderNum: String = smsMessage.originatingAddress ?: "unknown"
+                            val messageBody: String = smsMessage.messageBody ?: ""
+                            val timestamp = System.currentTimeMillis()
+
+                            runOnUiThread {
+                                updateUI(senderNum, messageBody, timestamp)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "onCreate")
 
-        smsTextView = findViewById(R.id.sms_text_view)
+        if (checkPermission()) {
+            registerSmsReceiver()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(smsReceiver)
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECEIVE_SMS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECEIVE_SMS),
+            SMS_PERMISSION_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                registerSmsReceiver()
+            } else {
+                // Handle permission denied
+                Log.e("MainActivity", "SMS permission denied")
+            }
+        }
+    }
+
+    private fun registerSmsReceiver() {
+        val intentFilter = IntentFilter("android.provider.Telephony.SMS_RECEIVED")
+        registerReceiver(smsReceiver, intentFilter)
+    }
+
+    private fun updateUI(senderNum: String, messageBody: String, timestamp: Long) {
+        Log.d("MainActivity", "Update UI with SMS content: $senderNum, $messageBody, $timestamp")
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val date = Date(timestamp)
+        val formattedDate = dateFormat.format(date)
+
+        val smsDetails = getString(R.string.sms_details, senderNum, messageBody, formattedDate)
+
+        val smsTextView: TextView = findViewById(R.id.sms_text_view)
+        smsTextView.append("\n$smsDetails")
     }
 }
--- app/src/main/java/com/myapp/myapplication/SMSReceiver.kt
+++ app/src/main/java/com/myapp/myapplication/SMSReceiver.kt
@@ -4,18 +4,41 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
-import android.widget.Toast
-
+import android.util.Log
+import android.telephony.SmsMessage
 
 class SMSReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context, intent: Intent) {
-        if(Telephony.Sms.Intents.SMS_RECEIVED_ACTION == intent?.action){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+            // Telephony.Sms.Intents.getMessagesFromIntent(intent) is used to retrieve an array of SmsMessage objects from the intent
             val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
-            for(sms in messages){
-                val messageBody = sms.messageBody
-                Toast.makeText(context, "New SMS Received: $messageBody", Toast.LENGTH_SHORT).show()
+
+            // Iterate over the array of SmsMessage to get the sender's phone number and the message body and the date
+            for (message in messages) {
+                // Handle the case where sender is null
+                val phoneNumber = message.displayOriginatingAddress ?: "unknown"
+                val timestamp = message.timestampMillis
+
+                // Use the alternative approach to retrieve the message body
+                val messageBody = getMessageBody(message)
+
+                Log.i(
+                    "SmsReceiver",
+                    "senderNum: $phoneNumber; message: $messageBody; timestamp: $timestamp"
+                )
+
+                // Show the SMS in the main activity using a global broadcast
+                val smsIntent = Intent("SMS_RECEIVED")
+                smsIntent.putExtra("senderNum", phoneNumber)
+                smsIntent.putExtra("messageBody", messageBody)
+                smsIntent.putExtra("timestamp", timestamp)
+                context?.sendBroadcast(smsIntent)
             }
         }
+    }
+
+    private fun getMessageBody(message: SmsMessage): String {
+        return message.displayMessageBody
     }
 }
